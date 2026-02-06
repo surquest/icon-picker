51e59f3ac78af667ad58611b4142a8d80fb56d18
Michal Švarc <michal.svarc@reality.co>
2026-02-06 19:26:08 +0100

    Add missing color to COLORS array in constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
-export const COLORS = ['#4D4D4D', '#bd2390', '#00a383', '#ee8101', '#cdb301'];
+export const COLORS = ['#4D4D4D', '#bd2390', '#00a383', '#ee8101', '#cdb301', '#2f5fa8'];
 
 export const CONFIG = {
     // Base path for the application resources.
